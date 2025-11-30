7a51c69e4431426dff8bcd87d4d151c04c941b39
MaxBahti <yozuka@mail.ru>
2025-11-30 18:03:37 +0400

    ifat3 is added

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.time.Duration;
+
+public class BaseTest {
+    WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--guest");
+
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+    }
+
+    @AfterMethod
+    public void close() {
+        // driver.quit();
+    }
+}
